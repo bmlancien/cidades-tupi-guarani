b09a73d853ec8b14fdb95a730ab085ae7f9dbe1f
Bryan <Bryan>
2021-02-27 11:57:39 +0100

    move files to new public folder

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.listen(8000, () => console.log('listening at 8000'));
+app.use(express.static('public'));
---
+++ public/js/project.js
@@ -0,0 +1,3 @@
+onMapLoaded(event) {
+    event.map.resize();
+}
