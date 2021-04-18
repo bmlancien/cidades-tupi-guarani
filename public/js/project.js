2970f6a7631c4e2c9c53798c4f6eadc92bf9bdd1
Bryan <Bryan>
2021-04-18 19:05:35 +0200

    add onboarding image and cta

--- public/js/project.js
+++ public/js/project.js
@@ -3,12 +3,14 @@
 // close onboarding
 let closeOnboarding = document.getElementById("js-close-onboarding");
 let onboardingModal = document.getElementById("js-onboarding-modal");
+let onboardingCTA = document.getElementById("js-onboarding-cta");
 
 function closeOnboardingModal() {
   onboardingModal.style.display = "none";
 }
 
 closeOnboarding.onclick = closeOnboardingModal;
+onboardingCTA.onclick = closeOnboardingModal;
 
 window.onclick = function(e) {
   if (e.target == onboardingModal) {
